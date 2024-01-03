0454f3d129492cc6b7b7380802a86673ce18c60a
Hiroyuki Kuromiya <15026387+kromiii@users.noreply.github.com>
2024-01-03 20:06:01 +0900

    Halo を使う

--- note_to_mp3.py
+++ note_to_mp3.py
@@ -3,28 +3,31 @@ import requests
 import sys
 from bs4 import BeautifulSoup
 from openai import OpenAI
+from halo import Halo
 
 def main():
-    print("Note ID: " + sys.argv[1])
-    base_url = "https://note.com/api/v3/notes/"
-    note_id = sys.argv[1]
-    url = base_url + note_id
-    response = requests.get(url)
-    print("記事をダウンロードしました")
-    data = json.loads(response.text)
-    soup = BeautifulSoup(data["data"]["body"], "html.parser")
-    for tag in soup.find_all("figure"):
-        tag.decompose()
-    text = soup.get_text("\n")
-    print("MP3に変換しています")
-    client = OpenAI()
-    response = client.audio.speech.create(
-        model="tts-1",
-        voice="alloy",
-        input=text,
-    )
-    response.stream_to_file(f"{note_id}.mp3")
-    print("完了しました")
+    with Halo(text='記事をダウンロードしています', spinner='dots') as sp:
+        base_url = "https://note.com/api/v3/notes/"
+        note_id = sys.argv[1]
+        url = base_url + note_id
+        response = requests.get(url)
+        sp.succeed()
+    with Halo(text='テキストを抽出しています', spinner='dots') as sp:
+        data = json.loads(response.text)
+        soup = BeautifulSoup(data["data"]["body"], "html.parser")
+        for tag in soup.find_all("figure"):
+            tag.decompose()
+        text = soup.get_text("\n")
+        sp.succeed()
+    with Halo(text='音声に変換しています', spinner='dots') as sp:
+        client = OpenAI()
+        response = client.audio.speech.create(
+            model="tts-1",
+            voice="alloy",
+            input=text,
+        )
+        response.stream_to_file(f"{note_id}.mp3")
+        sp.succeed()
 
 if __name__ == "__main__":
     main()
